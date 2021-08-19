bd4c5c1585a6816f88f9f799966ddc7a85eb6c4c
glauroqj <glauro.juliani@hotmail.com>
2021-08-19 11:58:14 -0300

    add: more files

--- src/index.js
+++ src/index.js
@@ -2,50 +2,55 @@ import express from 'express'
 import bodyParser from 'body-parser'
 /** services */
 import crawlService from './services/crawlService'
+/** routes */
+import routes from './routes/routes'
 
 const port = 5000
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({status: 'Server is running!'})
-})
+/** app routes */
+routes(app)
 
-app.post('/crawl', bodyParser.json(), (req, res) => {
-  const { routes, environment, folderName } = req?.body || false
+// app.get('/', (req, res) => {
+//   res.json({status: 'Server is running!'})
+// })
 
-  if (routes?.length > 0) {
+// app.post('/crawl', bodyParser.json(), (req, res) => {
+//   const { routes, environment, folderName } = req?.body || false
+
+//   if (routes?.length > 0) {
     
-    const queueService = routes.map((route, idx) => crawlService({url:route, environment, folderName, idx}))
+//     const queueService = routes.map((route, idx) => crawlService({url:route, environment, folderName, idx}))
     
-    Promise.allSettled([...queueService])
-    .then(values => {
+//     Promise.allSettled([...queueService])
+//     .then(values => {
       
-      console.log('< ROUTES > ', values)
-      res.status(200).send({
-        data: values
-      })
+//       console.log('< ROUTES > ', values)
+//       res.status(200).send({
+//         data: values
+//       })
 
-    })
-    .catch(e => {
-      res.status(500).send(e?.message || 'something get wrong!')
-    })
+//     })
+//     .catch(e => {
+//       res.status(500).send(e?.message || 'something get wrong!')
+//     })
 
 
-  } else {
-    res.status(500).send('something get wrong!')
-  }
-  //   try {
-  //     routes.map(async (url, idx) => await crawlService({url, environment, folderName, idx}))
-  //     console.log('\x1b[33m < SUCCESS > ', folderName, environment, routes?.length, routes, '\x1b[0m')
-  //     res.status(200).send('done!')
-  //   } catch(e) {
-  //     res.status(500).send(e)
-  //   }
-  // } else {
-  //   res.status(400).send('something get wrong!')
-  // }
+//   } else {
+//     res.status(500).send('something get wrong!')
+//   }
+//   //   try {
+//   //     routes.map(async (url, idx) => await crawlService({url, environment, folderName, idx}))
+//   //     console.log('\x1b[33m < SUCCESS > ', folderName, environment, routes?.length, routes, '\x1b[0m')
+//   //     res.status(200).send('done!')
+//   //   } catch(e) {
+//   //     res.status(500).send(e)
+//   //   }
+//   // } else {
+//   //   res.status(400).send('something get wrong!')
+//   // }
 
-})
+// })
 
 app.listen(port, () => {
   console.log(`Server is running at localhost:${port}`)
