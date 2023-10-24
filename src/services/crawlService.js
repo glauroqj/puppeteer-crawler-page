09cc8fe1a57b0e4b8c69a4cd2a8da48cdeaea529
glauroqj <glauro.juliani@hotmail.com>
2023-10-24 10:51:14 -0300

    fix: puppeteer version :  works again :)

--- src/services/crawlService.js
+++ src/services/crawlService.js
@@ -8,9 +8,9 @@ const crawlService = ({ url, environment, folderName, idx }) =>
 
     try {
       /** check folder */
-      if (!fs.existsSync(`./src/_data/screenshots/${folderName}`)) {
-        fs.mkdirSync(`./src/_data/screenshots/${folderName}`);
-      }
+      // if (!fs.existsSync(`./src/_data/screenshots/${folderName}`)) {
+      //   fs.mkdirSync(`./src/_data/screenshots/${folderName}`);
+      // }
 
       const browser = await puppeteer.launch({
         product: "chrome",
@@ -39,37 +39,66 @@ const crawlService = ({ url, environment, folderName, idx }) =>
 
       await page.goto(String(url), { waitUntil: "domcontentloaded" });
       // await page.emulate( iPhone6 )
-      await page.screenshot({
-        path: `./src/_data/screenshots/${folderName}/${environment + idx}.png`,
-        fullPage: true,
-      });
-
-      // Extract data from the website
-      // const data = await page.evaluate(() => {
-      //   // Customize this part to scrape the specific data you need from the website
-      //   const title = document.title;
-      //   const paragraphText = document.querySelector("p").textContent;
-
-      //   return {
-      //     title,
-      //     paragraphText,
-      //   };
+      // await page.screenshot({
+      //   path: `./src/_data/screenshots/${folderName}/${environment + idx}.png`,
+      //   fullPage: true,
       // });
 
-      // const jsonData = JSON.stringify(data, null, 2);
+      // Extract data from the website
+      const data = await page.evaluate(() => {
+        // Customize this part to scrape the specific data you need from the website
+        const titleRaw = document.querySelector(
+          "[data-testid='hero__pageTitle']"
+        );
+        const title = titleRaw.querySelector("span").textContent;
 
-      // fs.writeFile(
-      //   `./src/_data/json/${folderName}`,
-      //   jsonData,
-      //   "utf8",
-      //   (err) => {
-      //     if (err) {
-      //       console.error("Error writing JSON file:", err);
-      //     } else {
-      //       console.log(`JSON data saved to ${filePath}`);
-      //     }
-      //   }
-      // );
+        const descriptionRaw = document.querySelector(
+          'meta[name="description"]'
+        );
+        const description = descriptionRaw
+          ? descriptionRaw.getAttribute("content")
+          : null;
+        // {
+        //   id: '',
+        //   slug: '',
+        //   title: '',
+        //   description: '',
+        //   infos: {
+        //   "imdb": {
+        //     "link": "https://www.imdb.com/title/tt0073195/",
+        //     "rating": "8.1/10"
+        //   },
+        //   "year": 1945,
+        //   "country": "U.S.A",
+        //   "director": {
+        //     "link": "https://www.imdb.com/name/nm0000229/?ref_=tt_ov_dr",
+        //     "name": "Steven Spielberg"
+        //     }
+        //   }
+        // }
+
+        return {
+          title,
+          description,
+        };
+      });
+
+      console.log("< FINAL DATA > ", data);
+      const jsonData = JSON.stringify(data, null, 2);
+      const fileName = `${data?.title}`;
+
+      fs.writeFile(
+        `./src/_data/json/${folderName}/${fileName}`,
+        jsonData,
+        "utf8",
+        (err) => {
+          if (err) {
+            console.error("Error writing JSON file:", err);
+          } else {
+            console.log(`JSON data saved to ${fileName}`);
+          }
+        }
+      );
 
       await browser.close();
 
